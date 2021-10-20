b223cbb62ce505984916890f9e7d1b0b5f91a9b8
Marcus Morrow <marcusmorrow319@gmail.com>
2021-10-20 16:08:45 -0500

    song database set-up and displays on mp3 player.

--- jamming_gerald/jamming_gerald_app/models.py
+++ jamming_gerald/jamming_gerald_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import audioread
 
 # Create your models here.
+class Music(models.Model):
+    song_file = models.FileField(upload_to='music/')
+    song_name = models.CharField(default=song_file.name, max_length=200)
+    song_duration = models.CharField(default='0', max_length=200)
+    song_artist = models.CharField(default='no artist', max_length=200)
+    song_date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.song_name
+
+    def path(self):
+        return 'media/'+self.song_file.path
+    
+    def get_audio(self):
+        with audioread.audio_open(self.song_file.path) as f:
+            self.song_duration = f.duration
+        return self.song_duration
+    
+  
+class My_pictures(models.Model):
+      image = models.ImageField(upload_to='my_pics/')
+    
--- jamming_gerald/jamming_gerald_app/views.py
+++ jamming_gerald/jamming_gerald_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from .models import Music
+import audioread
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    my_songs = Music.objects.all()
+    songs = my_songs.order_by('song_date_added')
+    context = {
+        'songs':songs,
+    }
+  
+    return render(request, 'home.html', context)
+
+            
