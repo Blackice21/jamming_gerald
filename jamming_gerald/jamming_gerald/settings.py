0344ce3ec8bb0df1a9d0437c115ce2b76de55334
Marcus Morrow <marcusmorrow319@gmail.com>
2021-11-02 15:00:11 -0500

    fixed static folders

--- jamming_gerald/jamming_gerald/settings.py
+++ jamming_gerald/jamming_gerald/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfilesdir')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticdir')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
 MEDIA_URL = '/media/'
